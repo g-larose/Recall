cb1f24e78b9ff9d7046d2c422251ad0aa9ca7ebb
g-larose <larose.g@fromtier.com>
2024-11-19 18:22:10 -0500

    Add project files.

---
+++ Recall/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Recall;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.SetWindowSize(100, 30);
+        Display();
+        Console.ReadLine();
+    }
+
+    private static void Display()
+    {
+        var now = DateTime.UtcNow.ToString("MM/dd/yyy");
+        Console.WriteLine($"                         Welcome to RECALL    --daily reminder-- {now}");
+        Console.WriteLine("                         ==================================================");
+        Console.WriteLine();
+        Console.WriteLine("You have [0] reminders today");
+        Console.WriteLine();
+    }
+}
